8b5640314c71ffe1127be787968f1aa6fb21ebc3
Maria Soledade <maryaferreira94@gmail.com>
2021-04-20 17:40:18 -0300

    configurando cores, fontes, navegação entre telas

---
+++ src/screens/Welcome.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    Text, 
+    View,
+    Image, 
+    TouchableOpacity, 
+    StyleSheet,
+    Dimensions,
+} from 'react-native';
+import {Entypo} from '@expo/vector-icons'
+
+import wateringImg from '../assets/watering.png';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+import { useNavigation } from '@react-navigation/core';
+
+export function Welcome(){
+    const navigation = useNavigation();
+
+    function handleStart(){
+        navigation.navigate('UserIdentification')
+    }
+
+    return(
+        <SafeAreaView style={styles.container}>
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                Gerencie{'\n'}
+                suas plantas de {'\n'} 
+                forma fácil
+                </Text>
+
+                <Image 
+                    source={wateringImg}
+                    style={styles.image}
+                    resizeMode="contain"
+                />
+                
+                <Text style={styles.subtitle}>
+                Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar
+                você sempre que precisar.
+                </Text>
+
+                <TouchableOpacity
+                    style={styles.button} 
+                    activeOpacity={0.7}
+                    onPress={handleStart}
+                >
+                    <Text>
+                        <Entypo 
+                            name="chevron-right"
+                            style={styles.buttonIcon}
+                            />
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    },
+    wrapper:{
+        flex:1,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        padding: 20  
+    },
+    title:{
+        fontSize: 28,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading,
+        marginTop: 38,
+        fontFamily: fonts.heading,
+        lineHeight: 34
+    },
+    subtitle:{
+        textAlign: 'center',
+        fontSize: 18,
+        paddingHorizontal: 20,
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    image:{
+        height: Dimensions.get('window').width * 0.7
+    },
+    button:{
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 20,
+        height: 56,
+        width: 56,
+    },
+    buttonIcon:{
+        color: colors.white,
+        fontSize:32
+    }
+})
